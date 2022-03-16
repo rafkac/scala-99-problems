767dc4a0e47e56e0b40b2fb1f46d727a0ec60c34
rafalkac02 <rafalkaczynski02@gmail.com>
2022-03-17 00:10:03 +0100

    Solutions to problems

--- P08/P08.scala
+++ P08/P08.scala
@@ -1,20 +1,16 @@
 package P08
 
-object P08 extends App{
-
+object P08 extends App {
 
   def compress(ls: List[Char]): List[Char] = {
-    var result = List[Char]()
-    for (el <- ls) {
-      if (result.isEmpty || el != result.last) {
-        result = result :+ el
-      }
-    }
-    result
+
+    ls.foldLeft(List[Char]())({ (result, el) =>
+      if (result.isEmpty || el != result.last) result :+ el
+      else result
+    })
   }
 
   // example test
   val myList = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
   println(compress(myList))
-
 }

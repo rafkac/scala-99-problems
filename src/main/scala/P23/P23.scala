5539d0a546567c4fbf84ad36e275187552c7c8ee
rafalkac02 <69152315+rafalkac02@users.noreply.github.com>
2023-03-14 22:51:51 +0100

    P23 solution

--- src/main/scala/P23/P23.scala
+++ src/main/scala/P23/P23.scala
@@ -1,7 +1,15 @@
 package P23
 
-
 object P23 extends App {
 
-}
+  def removeAt[A](n: Int, ls: List[A]): (List[A], A) = {
+    (ls.filter(x => ls.indexOf(x) != n), ls(n))
+  }
 
+  def randomSelect[A](n: Int, ls: List[A]): List[A] = n match {
+    case n if n <= 0 => Nil
+    case _ =>
+      val (rest, e) = removeAt((new util.Random).nextInt(ls.length), ls)
+      e :: randomSelect(n - 1, rest)
+  }
+}
